78568f534353d4d9cc03ca16b58f7e6bf93bed99
cchaves1395 <carlos.chaves@3pillarglobal.com>
2022-07-13 11:22:04 -0600

    Initial Load
    Initial Load

---
+++ scripts/navigation.js
@@ -0,0 +1,30 @@
+var navigationContainer = document.getElementById("navigationContainer");
+var mobileToggle = document.getElementById("mobileMenu");
+
+const navigationItems = [
+    {name:"Solicitudes", url:"/Solicitud.html"},
+    {name: "Placeholder", url:"#"},
+    {name: "Placeholder", url:"#"}
+];
+
+var loadNavigation = ()=>{
+    var navigationHtml = "";
+    navigationItems.map((item)=>{
+        navigationHtml += `<li><a href="${item.url}">${item.name}</a>`
+    });
+    navigationContainer.insertAdjacentHTML('beforeend', navigationHtml);
+}
+
+loadNavigation();
+
+
+
+
+
+//Functions and loadings
+
+mobileToggle.addEventListener("click", ()=>{
+   
+    navigationContainer.classList.toggle("active");
+});
+
